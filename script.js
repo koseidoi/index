a5ade727901b444eeabcfc203a5156ad3ba16969
kosei-doi <113613042+kosei-doi@users.noreply.github.com>
2022-10-23 12:10:03 +0900

    fifth-commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch("https://kosei.doi.mydns.jp/data.json")
 .then(data => {
 	console.log(data);
 	let elem = document.getElementById("data");
-	elem.innerHtml = data.name + ":" + data.title; 
+	elem.innerText = data.name + ":" + data.title; 
 })
 .catch(error => {
 	console.log(error)
